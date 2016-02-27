e67ba4788beb029a25a4f9c48b29e721fb336f52
jyothisridhar <jyotishridhar7@gmail.com>
2016-02-27 19:49:52 +0530

    open only one infWindow

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-var map;
+
 var initialLocations = [{locationName: 'bowring institue, bangalore'},
 						{locationName: 'Church street Social, bangalore'},
 						{locationName: 'chutney chang, museum road, bangalore'},
@@ -7,77 +7,6 @@ var initialLocations = [{locationName: 'bowring institue, bangalore'},
 						{locationName: 'high court of karnataka, bangalore'},
 						{locationName: 'm chinnaswamy stadium, bangalore'}];
 var markers = [];
-
-function initMap() {
-	var self = this;
-
-	map = new google.maps.Map(document.getElementById('map-canvas'), {
-      center: {lat: 12.978825, lng: 77.599719},
-      zoom: 8
-    });
-    google.maps.event.addDomListener(window, "resize", function() {
-      var center = map.getCenter();
-      google.maps.event.trigger(map, "resize");
-      map.setCenter(center);
-    });
-
-    var bounds = new google.maps.LatLngBounds();
-
-    self.createMapMarker = function(placeData, setMarkerData){
-    	var lat = placeData.geometry.location.lat();  // latitude from the place service
-      	var lon = placeData.geometry.location.lng();  // longitude from the place service
-      	var name = placeData.name;   // name of the place from the place service
-
-      	// marker is an object with additional data about the pin for a single location
-      	var marker = new google.maps.Marker({
-	        map: map,
-	        position: placeData.geometry.location,
-	        title: name,
-	        animation: google.maps.Animation.DROP,
-      	});
-
-      	markers.push(marker);
-      	var infoWindow = new google.maps.InfoWindow({
-        	content: name
-      	});
-
-      	google.maps.event.addListener(marker, 'click', function() {
-	        marker.setAnimation(google.maps.Animation.BOUNCE);
-	        infoWindow.open(map, marker);
-	        setTimeout(function() {
-	        	marker.setAnimation(null)
-	        }, 1000);
-      	});
-
-      	if(typeof setMarkerData === "function") {
-      		setMarkerData(marker);
-      	}
-      	// this is where the pin actually gets added to the map.
-      	// bounds.extend() takes in a map location object
-      	bounds.extend(marker.position);
-      	// fit the map to the new marker
-      	map.fitBounds(bounds);
-      	// center the map
-      	map.setCenter(bounds.getCenter());
-    };
-
-    self.googlePlaceSearch = function(place, setGoogleData) {
-    	var service = new google.maps.places.PlacesService(map);
-
-    	var request = {
-        	query: place.locationName
-      	};
-      	// Actually searches the Google Maps API for location data and runs the callback
-      	// function with the search results after each search.
-      	service.textSearch(request, function(results, status) {
-	        if (status == google.maps.places.PlacesServiceStatus.OK) {
-	        	if(typeof setGoogleData === "function") {
-	            	setGoogleData(results[0]);
-	        	}
-	        }
-      	});
-    };
-};
 
 var Place = function(placeObj){
 	this.locationName = placeObj.locationName;
@@ -112,28 +41,28 @@ var ViewModel = function(mapView){
     	self.visibleLocations.push(place);
   	});
 
-  	this.showLocation = function(clickedLocation) {
-  		self.clearMarkers();
-  		mapView.createMapMarker(clickedLocation.placeServiceData);
-  	};
-
-  	this.clearMarkers = function(){
-    	self.setMapOnAll(null);
-  	};
-
   	this.setMapOnAll = function(map) {
 	    for (var i = 0; i < markers.length; i++) {
 	      markers[i].setMap(map);
 	    }
   	};
 
-  	this.onClick = function(clickedLocation){
+  	this.clearMarkers = function(){
+    	self.setMapOnAll(null);
+  	};
+
+  	this.animateMarker = function(clickedLocation){
   		clickedLocation.marker.setAnimation(google.maps.Animation.BOUNCE);
-  		// infoWindow.open(map, marker);
-	   //      setTimeout(function() {
-	   //      	marker.setAnimation(null)
-	   //  }, 1000);
-  	}
+	    setTimeout(function() {
+	      	marker.setAnimation(null)
+	    }, 1000);
+  	};
+
+  	this.showLocation = function(clickedLocation) {
+  		self.clearMarkers();
+  		mapView.createMapMarker(clickedLocation.placeServiceData);
+  		self.animateMarker(clickedLocation);
+  	};
 };
 
 function startApp(){
